110803262662bd3e57e71a6c5765ddbeef53cf9a
Marco-DLC <140775824+Marco-DLC@users.noreply.github.com>
2025-06-10 21:57:40 -0400

    Add Content component

---
+++ src/components/Content.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+export default function Content() {
+    return (
+        <div className='card'>
+            <h2>Online and In-person tutoring for</h2>
+        </div>
+    )
+}
